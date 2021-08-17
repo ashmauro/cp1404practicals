4000cf4c1ff3cb4b1c647f2823b064c2ce43899f
ashmauro <87747971+ashmauro@users.noreply.github.com>
2021-08-17 14:08:41 +1000

    finish broken_score.py

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,14 +2,30 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Obtain users score, or generate random score."""
+    score = float(input("Enter score: "))
+    result_grade = score_grade(score)
+    print(result_grade)
+
+    score = random.randint(0, 100)
+    result_grade = score_grade(score)
+    print(result_grade)
+
+
+def score_grade(score):
+    """Determine what grade the user got."""
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score > 90:
+        return "Excellent"
+    elif score > 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
