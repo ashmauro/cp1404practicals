63c0a156bfc7e037cb1cc8f2aab78604f19aff04
ashmauro <87747971+ashmauro@users.noreply.github.com>
2021-09-02 13:24:30 +1000

    finish emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,3 +2,30 @@
 CP1404/CP5632 Practical
 emails
 """
+
+
+def main():
+    """Create dictionary of emails to names."""
+    email_dict = {}
+    email = input("Email: ")
+    while email != "":
+        user_name = get_name(email)
+        user_confirmation = input("Is you name {}? (Y or N)".format(user_name))
+        if user_confirmation.upper() == "N" and user_confirmation != "":
+            user_name = input("Name: ")
+        email_dict[email] = user_name
+        email = input("Email: ")
+
+    for email, name in email_dict.items():
+        print("{} {}".format(name, email))
+
+
+def get_name(email):
+    """Extract and return name from email address."""
+    user_name = email.split("@")[0]
+    parts = user_name.split(".")
+    user_name = " ".join(parts).title()
+    return user_name
+
+
+main()
