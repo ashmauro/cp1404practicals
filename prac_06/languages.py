ea43bfa7c12bb1ad2cf7f3bd4af965d29716f248
ashmauro <87747971+ashmauro@users.noreply.github.com>
2021-09-07 14:41:07 +1000

    update docstring languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 Practical - programming language class.
+CP1404/CP5632 Practical - Client code to use the ProgrammingLanguage class.
 """
 
 from prac_06.programming_language import ProgrammingLanguage
