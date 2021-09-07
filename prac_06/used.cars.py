b611cf400e82a6aa991ba40a761819972e141a91
ashmauro <87747971+ashmauro@users.noreply.github.com>
2021-09-07 13:37:09 +1000

    finished car.py and used_cars.py

--- prac_06/used.cars.py
+++ prac_06/used.cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "my car")
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -14,6 +14,15 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car(100, "Limo")
+    limo.fuel += 20
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
+
+    print(limo)
+    print(my_car)
 
 
 main()
