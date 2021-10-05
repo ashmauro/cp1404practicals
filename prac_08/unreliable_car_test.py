86a6977a4b3fd50cf0501e858737b471bc91cf23
ashmauro <87747971+ashmauro@users.noreply.github.com>
2021-10-05 14:31:05 +1000

    update unreliable_car_test.py

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -9,9 +9,12 @@ from prac_08.unreliable_car import UnreliableCar
 def main():
     """Code to test UnreliableCar class."""
 
-    car = UnreliableCar("Unreliable 1", 100, 50)
-    car.drive(40)
-    print(car)
+    good_car = UnreliableCar("More reliable", 100, 92)
+    bad_car = UnreliableCar("Less reliable", 100, 8)
+    good_car.drive(40)
+    bad_car.drive(40)
+    print(good_car)
+    print(bad_car)
 
 
 main()
